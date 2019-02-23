878f2332a9fafc5acfc6477df948b6430989f20e
metajs <zycode277@gmail.com>
2019-02-23 18:26:02 +0800

    update: README

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Centos boot script
 
+- git wget
+- docker
+- docker-composer
+
+
 # TODO
 - [ ]boost when some command are installed
+
+# install
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/broven/centos-boot.sh/master/install.sh -o install.sh
+bash install.sh
+```
--- install.sh
+++ install.sh
@@ -27,19 +27,12 @@ install_docker_composer() {
     sudo pip install docker-compose
 }
 
-run_frp() {
-    mkdir /var/frp && mkdir /var/frp/conf &&cd /var/frp/conf
-    wget https://frp.anys.ga/frps.ini
-    sudo docker run --name frps -d -v /var/frp/conf:/var/frp/conf -p 5000-5100:5000-5100 -p 7000:7000 -p 7500:7500 -p 7001:7001 -p 8089:80 -p 8443:443 ruiny/frps
-}
-
 
 boot() {
     yum update -y
     install_utils
     install_docker
     install_docker_composer
-    run_frp
 }
 
 boot
